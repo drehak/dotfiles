44bada7aed734246dd1ba66a59858def78016e08
Dominik Rehák <drehak@redhat.com>
2020-01-14 20:24:07 +0100

    disable tput calls if tput is not present
    where might tput not be present? clean termux, for instance

--- setup.sh
+++ setup.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-I=$(tput bold)$(tput setaf 2)  # installed - green
-S=$(tput bold)$(tput setaf 3)  # skipped - yellow
-F=$(tput bold)$(tput setaf 1)  # failed - red
-R=$(tput sgr0)                 # reset
+if command -v tput >/dev/null; then
+    I=$(tput bold)$(tput setaf 2)  # installed - green
+    S=$(tput bold)$(tput setaf 3)  # skipped - yellow
+    F=$(tput bold)$(tput setaf 1)  # failed - red
+    R=$(tput sgr0)                 # reset
+    alias echo="echo -e"
+else
+    I=":) "
+    S="   "
+    F=":( "
+fi
 
 count_I=0
 count_S=0
@@ -13,10 +20,10 @@ backup() {
     # copies file at $1 to first suitable filename
     # return: 0 = installed, 1 = failed, 2 = skipped, 3 = doesn't exist
     if [ ! -e "$1" ]; then
-        echo -e >&2 "$S$1: file doesn't exist - skipping backup$R"
+        echo >&2 "$S$1: file doesn't exist - skipping backup$R"
         return 3
     elif [ ! -r "$1" ]; then
-        echo -e >&2 "$F$1: can't read file$R"
+        echo >&2 "$F$1: can't read file$R"
         return 1
     fi
 
@@ -31,17 +38,17 @@ backup() {
         esac
         if [ ! -e "$dst" ]; then
             if [ -n "$last" ] && diff -q "$1" "$last" >/dev/null; then
-                # echo -e >&2 "$S$1: last backup $last does not differ - skipping backup$R"
+                # echo >&2 "$S$1: last backup $last does not differ - skipping backup$R"
                 return 2
             fi
             cp "$1" "$dst"
-            echo -e >&2 "$I$1: backed up to $dst$R"
+            echo >&2 "$I$1: backed up to $dst$R"
             return 0
         fi
         local last="$dst"
     done
         
-    echo -e >&2 "$F$1: could not backup - exhausted suffixes?$R"
+    echo >&2 "$F$1: could not backup - exhausted suffixes?$R"
     return 1
 }
 
@@ -49,7 +56,7 @@ install() {
     # copies file at $1 to $2
     # return: 0 = installed, 1 = failed, 2 = skipped
     if [ ! -r "$1" ]; then
-        echo -e >&2 "$F$1: can't read file$R"
+        echo >&2 "$F$1: can't read file$R"
         return 1
     fi
 
@@ -58,8 +65,8 @@ install() {
         return 1
     fi
 
-    cp "$1" "$2" || { echo -e >&2 "$F$1: failed to install to $2$R"; return 1; }
-    echo -e >&2 "$I$1: installed to $2$R"
+    cp "$1" "$2" || { echo >&2 "$F$1: failed to install to $2$R"; return 1; }
+    echo >&2 "$I$1: installed to $2$R"
     return 0
 }
 
@@ -70,7 +77,7 @@ install_if_exists() {
         install "$2" "$3"
         return "$?"
     else
-        echo -e >&2 "$S$2: $1 is not installed, skipping$R"
+        echo >&2 "$S$2: $1 is not installed, skipping$R"
         return 2
     fi
 }
