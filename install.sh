d0e9cd63856582aec24ab7751db5e9e15c80ac63
Dominik Rehák <drehak@redhat.com>
2020-02-06 13:35:57 +0100

    add git config

--- install.sh
+++ install.sh
@@ -3,4 +3,5 @@
 # install_if_exists: copies file at $2 to $3 if the command at $1 exists
 install_if_exists  bash      files/bashrc         ~/.bashrc
 install_if_exists  vim       files/vimrc          ~/.vimrc
+install_if_exists  git       files/gitconfig      ~/.gitconfig
 install_if_exists  redshift  files/redshift.conf  ~/.redshift.conf
