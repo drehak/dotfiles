29e2fd430bd0e8904aef78e4ccb1920b4334a95e
Dominik Rehák <drehak@redhat.com>
2020-02-18 12:17:42 +0100

    aisa declarations, tmux.conf

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # install: copies file at $1 to $2
 # install_if_exists: copies file at $2 to $3 if the command at $1 exists
 install_if_exists  bash      files/bashrc         ~/.bashrc
-install_if_exists  vim       files/vimrc          ~/.vimrc
 install_if_exists  git       files/gitconfig      ~/.gitconfig
 install_if_exists  redshift  files/redshift.conf  ~/.redshift.conf
+install_if_exists  tmux      files/tmux.conf      ~/.tmux.conf
+install_if_exists  vim       files/vimrc          ~/.vimrc
