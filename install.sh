3909c4a8bf87d9a028a80f6edd5b22b8a610ed9f
Dominik Rehák <drehak@redhat.com>
2021-02-15 22:40:00 +0100

    polybar: a bar for each workspace (+ launch script)

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 # install: copies file at $1 to $2
 # install_if_exists: copies file at $2 to $3 if the command at $1 exists
-install_if_exists  bash         files/bashrc           ~/.bashrc
-install_if_exists  git          files/gitconfig        ~/.gitconfig
-install_if_exists  polybar      files/polybar/config   ~/.config/polybar/config
-install_if_exists  polybar      files/polybar/cmus.sh  ~/.config/polybar/cmus.sh
-install_if_exists  qutebrowser  files/qutebrowser.py   ~/.config/qutebrowser/config.py
-install_if_exists  redshift     files/redshift.conf    ~/.redshift.conf
-install_if_exists  tmux         files/tmux.conf        ~/.tmux.conf
-install_if_exists  vim          files/vimrc            ~/.vimrc
+install_if_exists  bash         files/bashrc                     ~/.bashrc
+install_if_exists  git          files/gitconfig                  ~/.gitconfig
+install_if_exists  polybar      files/polybar/config             ~/.config/polybar/config
+install_if_exists  polybar      files/polybar/cmus.sh            ~/.config/polybar/cmus.sh
+install_if_exists  polybar      files/polybar/launch_polybar.sh  ~/launch_polybar.sh
+install_if_exists  qutebrowser  files/qutebrowser.py             ~/.config/qutebrowser/config.py
+install_if_exists  redshift     files/redshift.conf              ~/.redshift.conf
+install_if_exists  tmux         files/tmux.conf                  ~/.tmux.conf
+install_if_exists  vim          files/vimrc                      ~/.vimrc
