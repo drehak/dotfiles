d4d9a5bc9540327ed8fecba321ea67cbbda1b565
Dominik Rehák <drehak@redhat.com>
2021-03-01 22:40:28 +0100

    task basics

--- install.sh
+++ install.sh
@@ -8,5 +8,6 @@ install_if_exists  polybar      files/polybar/cmus.sh            ~/.config/polyb
 install_if_exists  polybar      files/polybar/launch_polybar.sh  ~/launch_polybar.sh
 install_if_exists  qutebrowser  files/qutebrowser.py             ~/.config/qutebrowser/config.py
 install_if_exists  redshift     files/redshift.conf              ~/.redshift.conf
+install_if_exists  task         files/taskrc                     ~/.taskrc
 install_if_exists  tmux         files/tmux.conf                  ~/.tmux.conf
 install_if_exists  vim          files/vimrc                      ~/.vimrc
