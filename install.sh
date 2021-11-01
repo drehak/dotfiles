9088db12160cf2a0ba963d2de4a7ee4a8004cc0c
Dominik Rehák <drehak@redhat.com>
2021-11-01 13:12:10 +0100

    move launch_polybar.sh to ~/scripts
    TODO find a way to create necessary directories (like here)

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ install_if_exists  bash         files/bashrc                     ~/.bashrc
 install_if_exists  git          files/gitconfig                  ~/.gitconfig
 install_if_exists  polybar      files/polybar/config             ~/.config/polybar/config
 install_if_exists  polybar      files/polybar/cmus.sh            ~/.config/polybar/cmus.sh
-install_if_exists  polybar      files/polybar/launch_polybar.sh  ~/launch_polybar.sh
+# TODO find a way to create necessary directories (like here)
+install_if_exists  polybar      files/polybar/launch_polybar.sh  ~/scripts/launch_polybar.sh
 install_if_exists  qutebrowser  files/qutebrowser/config.py      ~/.config/qutebrowser/config.py
 install_if_exists  qutebrowser  files/qutebrowser/blockedHosts   ~/.config/qutebrowser/blockedHosts
 install_if_exists  redshift     files/redshift.conf              ~/.config/redshift.conf
