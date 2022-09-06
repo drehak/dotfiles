94e3cebeb93f8f27688ba712823e2b5254d73d39
Dominik Rehák <drehak@redhat.com>
2022-09-06 10:32:37 +0100

    polybar: wttr.in: enable additional error check

--- files/polybar/wttr.sh
+++ files/polybar/wttr.sh
@@ -5,5 +5,5 @@ output=$(curl -s wttr.in/$location?format=%t)
 code=$?
 
 [ $code != 0 ] && echo "curl: $code" && exit 1
-# [ $(echo "$output" | wc -l) != 1 ] && echo "error?" && exit 1
+[ $(echo "$output" | wc -l) != 1 ] && echo "error?" && exit 1
 echo "$output"
