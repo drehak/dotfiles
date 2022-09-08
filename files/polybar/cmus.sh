0edc968a6e2693e4379d6af5701c1275c5b420ce
Dominik Rehák <drehak@redhat.com>
2022-09-08 10:45:04 +0100

    polybar: cmus: trim long song names

--- files/polybar/cmus.sh
+++ files/polybar/cmus.sh
@@ -2,13 +2,17 @@
 
 cmus-remote -C status | grep stopped >/dev/null && exit 1
 info=$(cmus-remote -C status)
+max_song_length=40
 
 prepend_zero () {
     seq -f "%02g" $1 $1
 }
 
-artist=$(echo -n $(echo "$info" | grep "tag artist" -m 1| cut -d ' ' -f 3-))
-song=$(echo -n $(echo "$info" | grep "tag title" | cut -d ' ' -f 3-))
+artist=$(echo -n $(echo "$info" | grep "tag artist" -m 1 | cut -d ' ' -f 3-))
+song=$(echo -n $(echo "$info" | grep "tag title" -m 1 | cut -d ' ' -f 3-))
+if [ ${#song} -gt $max_song_length ]; then
+    song="$(echo $song | cut -c1-$max_song_length | xargs)…"
+fi
 
 position=$(echo "$info" | grep position | cut -c 10-)
 minutes1=$(prepend_zero $(($position / 60)))
